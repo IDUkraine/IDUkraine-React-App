91589f35c0d06531e551daec643f59e49bc10e21
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2025-04-30 17:38:28 +0300

    feat: add general news section

--- idukraine-react/src/components/pages/home/HomePage.tsx
+++ idukraine-react/src/components/pages/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeroSection from './Hero/HeroSection';
 import AboutSection from './About/AboutSection';
 import AreasSection from './Areas/Areas';
+import GeneralNewsSection from './GeneralNews/GeneralNewsSection';
 const HomePage = () => {
   return (
     <div className="home-page">
       <HeroSection />
       <AboutSection />
+      <GeneralNewsSection />
       <AreasSection />
     </div>
   );
