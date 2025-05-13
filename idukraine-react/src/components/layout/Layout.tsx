8673629881dfaf7d0baeda14f6bff8a008c0ecd9
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2025-05-13 14:18:34 +0300

    fix: small fix

--- idukraine-react/src/components/layout/Layout.tsx
+++ idukraine-react/src/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import '../../assets/styles/layout.css';
